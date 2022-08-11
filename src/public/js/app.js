5562eb1e066d4d76f09610492b8962e324cb9544
ekkim <shinhwa069@naver.com>
2022-08-12 00:46:27 +0900

    day 4 of 7

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,8 +2,10 @@ const socket = io();
 const welcome = document.getElementById('welcome');
 const form = welcome.querySelector('form');
 const room = document.getElementById('room');
-
-let roomName;
+const currentNickname = document.querySelector('#currentNickname');
+const roomName = welcome.querySelector('#roomName');
+let roomname;
+room.hidden = true;
 
 function addMessage(message) {
     const ul = room.querySelector('ul');
@@ -13,35 +15,64 @@ function addMessage(message) {
 }
 function handleMessageSubmit(event) {
     event.preventDefault();
-    const input = room.querySelector('input');
+    const input = room.querySelector('#msg input');
     const value = input.value;
-    socket.emit('new_message', input.value, roomName, () => {
+    socket.emit('new_message', input.value, roomname, () => {
         addMessage(`You: ${value}`);
     });
     input.value = '';
 }
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const input = welcome.querySelector('#name input');
+    currentNickname.innerText = `your current nickname is ✨ ${input.value} ✨`;
+    socket.emit('nickname', input.value);
+    const btn = form.querySelector('#name button');
+    btn.textContent = 'change';
+}
+
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
     const h3 = room.querySelector('h3');
-    h3.innerText = `Room ${roomName}`;
-    const form = room.querySelector('form');
-    form.addEventListener('submit', handleMessageSubmit);
+    h3.innerText = `Room ${roomname}`;
+    const msgForm = room.querySelector('#msg');
+    msgForm.addEventListener('submit', handleMessageSubmit);
 }
+
 function handleRoomSubmit(event) {
     event.preventDefault();
-    const input = form.querySelector('input');
+    const input = roomName.querySelector('input');
     socket.emit('enter_room', input.value, showRoom);
-    roomName = input.value;
     // Websocket은 String만 전송할수있어서 타입을 바꿔줘야 했는데, socketio는 Object를 보낼 수 있어서 타입변환이 필요없다
+    roomname = input.value;
     input.value = '';
 }
-form.addEventListener('submit', handleRoomSubmit);
 
-socket.on('welcome', () => {
-    addMessage('someone joined!');
+roomName.addEventListener('submit', handleRoomSubmit);
+const nameForm = welcome.querySelector('#name');
+nameForm.addEventListener('submit', handleNicknameSubmit);
+
+socket.on('welcome', (user, newCount) => {
+    const h3 = room.querySelector('h3');
+    h3.innerText = `Room ${roomname} (${newCount})`;
+    addMessage(`${user} arrived!`);
 });
-socket.on('bye', () => {
-    addMessage('someone left ㅠㅠ!');
+socket.on('bye', (user, newCount) => {
+    const h3 = room.querySelector('h3');
+    h3.innerText = `Room ${roomname} (${newCount})`;
+    addMessage(`${user} left ㅠㅠ!`);
 });
 socket.on('new_message', addMessage);
+socket.on('room_change', (rooms) => {
+    const roomList = welcome.querySelector('ul');
+    roomList.innerHTML = '';
+    if (rooms.length === 0) {
+        return;
+    }
+    rooms.forEach((room) => {
+        const li = document.createElement('li');
+        li.innerText = room;
+        roomList.append(li);
+    });
+});
--- src/server.js
+++ src/server.js
@@ -14,22 +14,52 @@ app.get('/*', (req, res) => res.redirect('/'));
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: { sids, rooms },
+        },
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on('connection', (socket) => {
+    socket['nickname'] = 'anonymous';
     socket.onAny((event) => {
+        console.log(wsServer.sockets.adapter);
         console.log(`Socket Event : ${event}`);
     });
     socket.on('enter_room', (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit('welcome');
+        socket
+            .to(roomName)
+            .emit('welcome', socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit('room_change', publicRooms());
     });
     socket.on('disconnecting', () => {
-        socket.rooms.forEach((room) => socket.to(room).emit('bye'));
+        socket.rooms.forEach((room) =>
+            socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1)
+        );
+    });
+    socket.on('disconnect', () => {
+        wsServer.sockets.emit('room_change', publicRooms());
     });
     socket.on('new_message', (msg, room, done) => {
-        socket.to(room).emit('new_message', msg);
+        socket.to(room).emit('new_message', `${socket.nickname} : ${msg}`);
         done();
     });
+    socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
 });
 
 // ws만 만들고 싶으면 http는 없어도 된다(선택사항)
